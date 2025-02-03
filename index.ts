bffa7e9f3f5f37291550d72b2e550143a111b6c2
Maria F. Barrero R. <mfbarrero@ccxc.co>
2025-02-03 10:16:26 -0500

    More changes

--- index.ts
+++ index.ts
@@ -5,3 +5,7 @@ console.log(`La resta de 10 - 3 es: ${diff(10, 3)}`);
 const sum = (a: number, b: number): number => a + b;
 
 console.log(`La suma de 15 + 3 es: ${diff(15, 3)}`);
+
+const mod = (a: number, b: number): number => a % b;
+
+console.log(`El resto de 4 sobre 2 es: ${diff(4, 2)}`);
