81395117b5d85a6ae5a2072d0de4764684ebd859
Maria F. Barrero R. <mfbarrero@ccxc.co>
2025-02-03 10:15:07 -0500

    Add more change

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 const diff = (a: number, b: number): number => a - b;
 
 console.log(`La resta de 10 - 3 es: ${diff(10, 3)}`);
+
+const sum = (a: number, b: number): number => a + b;
+
+console.log(`La suma de 15 + 3 es: ${diff(15, 3)}`);
