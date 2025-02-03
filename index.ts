f1fcb9e32a82a353ba9304f4059400da69ec1655
Maria F. Barrero R. <mfbarrero@ccxc.co>
2025-02-03 10:25:03 -0500

    Fix func

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 const sum = (a: number, b: number): number => a + b;
 
-console.log(`La suma de 10 + 3 es: ${sum(10, 3)}`);
+console.log(`La suma de 8 + 7 es: ${sum(8, 7)}`);
+
+const div = (a: number, b: number): number => a / b;
+
+console.log(`La división de 81 / 9 es: ${sum(81, 9)}`);
