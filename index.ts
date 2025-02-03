a71182a964cf50097a1fe702364e9e0c5f5ae17e
MARÍA FERNANDA BARRERO RINCÓN <59446078+MariaFerCCxC@users.noreply.github.com>
2025-02-03 10:31:20 -0500

    Main 1 (#2)
    * Change base file
    
    * Add more change
    
    * More changes

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
+const diff = (a: number, b: number): number => a - b;
+
+console.log(`La resta de 10 - 3 es: ${diff(10, 3)}`);
+
 const sum = (a: number, b: number): number => a + b;
 
 console.log(`La suma de 8 + 7 es: ${sum(8, 7)}`);
+
+const mod = (a: number, b: number): number => a % b;
+
+console.log(`El resto de 4 sobre 2 es: ${diff(4, 2)}`);
 
 const div = (a: number, b: number): number => a / b;
 
